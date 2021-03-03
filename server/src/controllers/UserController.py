de2d806ec13a74604764b64ba7688cd1cfc6539c
Matheus De Assis <65235458+MatheusAssisM@users.noreply.github.com>
2021-03-03 12:29:21 -0300

    updated docstrings

--- server/src/controllers/UserController.py
+++ server/src/controllers/UserController.py
@@ -5,6 +5,8 @@ import json
 
 class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
+        """Hadle to HTTP GET
+        """
         try:
             self._set_headers(200)
 
@@ -18,34 +20,52 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             return
 
     def do_POST(self):
-        pararm = self.getUrlPararm(self.path)
-        if self.path == "/":
-            self._set_headers(201)
+        """handle two routes from POST
+        """
+        try:
+            pararm = self.getUrlPararm(self.path)
+            if self.path == "/":
+                self._set_headers(201)
 
-            postRequest = self.rfile.read(int(self.headers['Content-Length']))
+                postRequest = self.rfile.read(int(self.headers['Content-Length']))
 
-            formData = self.cleanPostData(postRequest)
-            self.createPessoa(formData)
+                formData = self.cleanPostData(postRequest)
+                self.createPessoa(formData)
 
-        elif self.path[1::] == pararm:
-            isExistsPessoa = self.findById(pararm)
-            if isExistsPessoa:
-                self._set_headers(204)
-                self.deleteById(pararm)
-                
-                return
+            elif self.path[1::] == pararm:
+                isExistsPessoa = self.findById(pararm)
+                if isExistsPessoa:
+                    self._set_headers(204)
+                    self.deleteById(pararm)
+                    return
+                else:
+                    self._set_headers(400)
+                    return
             else:
                 self._set_headers(400)
                 return
-        else:
-            self._set_headers(400)
+        except:
+            self._set_headers(500)
             return
 
     def getUrlPararm(self, path):
+        """get pararm from URL
+
+        Args:
+            path (str): route from request
+
+        Returns:
+            str: the pararm
+        """
         response = path[1::]
         return response
 
     def getDataToJinja(self):
+        """get all data that will be send to Jinja template
+
+        Returns:
+            object: all data
+        """
         date, data = self.listAll()
         data = {
             "title": "Visie - Teste Prático",
@@ -57,6 +77,11 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         return data
 
     def listAll(self):
+        """list all row on table
+
+        Returns:
+            list: list with all rows
+        """
         pessoa = PessoaService()            
         response = pessoa.getAll()
         formatedDate = []
@@ -74,11 +99,29 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         return formatedDate, response 
 
     def cleanPostData(self, post):
+        """receive and clean post data
+
+        Args:
+            post (bytes): data from request
+
+        Returns:
+            dict: formated data
+        """
         data = post.decode()
         myDict = json.loads(data)        
         return myDict
 
     def getTemplate(self, data, file, dir):
+        """render the jinja template
+
+        Args:
+            data (dict): dict with all variables
+            file (str): path
+            dir (str): dir
+
+        Returns:
+            any: return rendered template
+        """
         fileLoader = FileSystemLoader(dir)
         env = Environment(loader=fileLoader)
         template = env.get_template(file)
@@ -86,32 +129,62 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         return self.wfile.write(output.encode())
 
     def _set_headers(self, code):
+        """Set headers to response
+
+        Args:
+            code (int): a HTTP code
+        """
         self.send_response(code)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
     def createPessoa(self, obj):
+        """send the pessoa obj to be saved on DB
+
+        Args:
+            obj (object): pessoa object
+        """
         pessoa = PessoaService()
         pessoa.create(obj)
         return
 
     def deleteById(self, id):
+        """delete a pessoa by id
+
+        Args:
+            id (string): prymary key of pessoa
+        """
         pessoa = PessoaService()
         pessoa.deleteById(id)
         return
     
     def deleteAll(self):
+        """Delete all data from table
+        """
         pessoa = PessoaService()
         pessoa.deleteAll()
         return 
 
 
     def getAllCollumnsNames(self):
+        """get columns names and types
+
+        Returns:
+            list: response from DB
+        """
         pessoa = PessoaService()            
         response = pessoa.getCollumnsNames()
         return response
     
     def findById(self, id):
+        """find a row by id
+
+        Args:
+            id (str): row id
+
+        Returns:
+            list: list with all informations about the row
+        """
         pessoa = PessoaService()
         response =  pessoa.findOne(id)
         return response
